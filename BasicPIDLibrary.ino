7a4437565645311dacc1b62e7af6b49a3a3de3fc
JW <jwilhelm@ohio.edu>
2018-10-31 11:55:06 -0400

    Removed extra var

--- BasicPIDLibrary.ino
+++ BasicPIDLibrary.ino
@@ -5,18 +5,14 @@
 #include "BasicPIDLibraryClass.h"
 #define PIN_INPUT 0
 #define PIN_OUTPUT 3
-
-double gSetpoint;
+double gSetpoint=100.0;
 double Kp=1.0, Ki=0.001, Kd=0.001;
 BasicPIDLibrary myPID(Kp, Ki, Kd);
-
 void setup()
 {
-  gSetpoint = 100;
   myPID.SetOutputLimits(-1,1);
   myPID.SetSampleTime(100);
 }
-double t = 0;
 void loop()
 {
   static double dOutput=0.0;
